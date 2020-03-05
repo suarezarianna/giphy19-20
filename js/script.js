ea3887a199db5aaf2512d32b95b7cb4ce977f661
suarezarianna <suarezarianna70@gmail.com>
2020-03-05 02:26:08 +0000

    changed java script

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@
 
 console.log('hi');
 
-$('h1').click(function(){
-    $('h1').css('color', 'green');
-    $('h1').text('Ready to Code');
+$('button').click(function(){
+  let message = $("input").val();
+  fetch("https://api.giphy.com/v1/gifs/search?&api_key=wr5FTD48d1jb75Qn4rg9zh7nF9D93cpi&rating=pg&q=puppy")
+  .then(function(response) {
+    return response.json();
+  })  
+  .then(function(data) {
+    
+    var url = data.data[0].images.original.url;
+    $(".displayGIF").append(`<img src="${url}">`);
+  });
 });
